8c6ad034907b5695a3ba183b862abb2697cbb9f8
Irimo <irimodi@gmail.com>
2020-11-20 10:18:27 +0900

    コンパイルを通した

--- output/bundle.js
+++ output/bundle.js
@@ -10,30 +10,30 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/main.ts":
-/*!*********************!*\
-  !*** ./src/main.ts ***!
-  \*********************/
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
 /*! namespace exports */
 /*! exports [not provided] [no usage info] */
 /*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sub__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sub */ \"./src/sub.ts\");\n\nvar Main = /** @class */ (function () {\n    function Main() {\n        var _this = this;\n        this.sub = new _sub__WEBPACK_IMPORTED_MODULE_0__.Sub();\n        var body = document.getElementsByTagName('body');\n        var element = body.item(0);\n        var div = document.createElement('div');\n        div.innerHTML = '「こんにちは」を他言語に変換します。<br>好きな言語を入れてください。<br>※ちなみに対応しているのは日本語、英語、スペイン語、ドイツ語です。<br>';\n        var input = document.createElement('input');\n        input.id = 'country';\n        var button = document.createElement('button');\n        button.textContent = '変換';\n        button.addEventListener('click', function () {\n            _this.transtrationGreeting(document.getElementById('country')['value']);\n        });\n        div.appendChild(input);\n        div.appendChild(button);\n        element.appendChild(div);\n    }\n    Main.prototype.transtrationGreeting = function (country) {\n        var addDiv = document.createElement('div');\n        addDiv.innerHTML = \"\\u3053\\u3093\\u306B\\u3061\\u306F\\u3092\" + country + \"\\u3067\\u8A00\\u3046\\u3068\" + this.sub.hello(country);\n        document.getElementsByTagName('body').item(0).appendChild(addDiv);\n    };\n    return Main;\n}());\nvar main = new Main();\n\n\n//# sourceURL=webpack:///./src/main.ts?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sortanima__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sortanima */ \"./src/sortanima.ts\");\n\ndocument.addEventListener('DOMContentLoaded', function () {\n    //カンバスをセット\n    var canvas = document.getElementById('canvas');\n    var sortanima = new _sortanima__WEBPACK_IMPORTED_MODULE_0__.SortAnima(canvas);\n});\n\n\n//# sourceURL=webpack:///./src/index.ts?");
 
 /***/ }),
 
-/***/ "./src/sub.ts":
-/*!********************!*\
-  !*** ./src/sub.ts ***!
-  \********************/
+/***/ "./src/sortanima.ts":
+/*!**************************!*\
+  !*** ./src/sortanima.ts ***!
+  \**************************/
 /*! namespace exports */
-/*! export Sub [provided] [no usage info] [missing usage info prevents renaming] */
+/*! export SortAnima [provided] [no usage info] [missing usage info prevents renaming] */
 /*! other exports [not provided] [no usage info] */
 /*! runtime requirements: __webpack_require__.r, __webpack_exports__, __webpack_require__.d, __webpack_require__.* */
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Sub\": () => /* binding */ Sub\n/* harmony export */ });\nvar Sub = /** @class */ (function () {\n    function Sub() {\n        this.greeting = {\n            '日本語': 'こんにちは',\n            '英語': 'Hello',\n            'スペイン語': 'Hola',\n            'ドイツ語': 'Hallo'\n        };\n    }\n    Sub.prototype.hello = function (country) {\n        return this.greeting[country];\n    };\n    return Sub;\n}());\n\n\n\n//# sourceURL=webpack:///./src/sub.ts?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"SortAnima\": () => /* binding */ SortAnima\n/* harmony export */ });\nvar SortAnima = /** @class */ (function () {\n    // const roulette = new Roulette(canvas, 500);\n    function SortAnima(canvas) {\n        this.canvas = document.getElementById('canvas');\n        //カンバスが使用できるかチェック\n        if (!canvas.getContext) {\n            console.log('[Roulette.constructor] カンバスが使用できません');\n            // this.enable = false;\n            return;\n        }\n        this.canvas = canvas;\n        this.ctx = canvas.getContext('2d');\n        // this.sub = new Sub();\n        // const body = document.getElementsByTagName('body');\n        // const element = body.item(0);\n        // const div = document.createElement('div');\n        // div.innerHTML = '「こんにちは」を他言語に変換します。<br>好きな言語を入れてください。<br>※ちなみに対応しているのは日本語、英語、スペイン語、ドイツ語です。<br>';\n        // const input = document.createElement('input');\n        // input.id = 'country'\n        // const button = document.createElement('button');\n        // button.textContent = '変換';\n        // button.addEventListener('click', () => {\n        //     this.transtrationGreeting(document.getElementById('country')['value']);\n        // });\n        // div.appendChild(input);\n        // div.appendChild(button);\n        // element.appendChild(div);\n    }\n    return SortAnima;\n}());\n\n// const main = new Main()\n\n\n//# sourceURL=webpack:///./src/sortanima.ts?");
 
 /***/ })
 
@@ -94,7 +94,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /************************************************************************/
 /******/ 	// startup
 /******/ 	// Load entry module
-/******/ 	__webpack_require__("./src/main.ts");
+/******/ 	__webpack_require__("./src/index.ts");
 /******/ 	// This entry module used 'exports' so it can't be inlined
 /******/ })()
 ;
